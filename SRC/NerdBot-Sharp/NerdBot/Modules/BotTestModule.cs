f3134e9939c7069a2a2e58fc5652656e004c6552
Eleanor Ashton <2723221+ElaTheDeveloper@users.noreply.github.com>
2021-05-09 18:09:41 +0100

    Fixed all names

---
+++ SRC/NerdBot-Sharp/NerdBot/Modules/BotTestModule.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+namespace NerdBot.Modules
+{
+    public class BotTestModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Summary("Just here if you want to check NerdBot is still alive.")]
+        [Alias("keepalive", "areyouthere")]
+        public async Task SendPingAsync()
+        {
+           
+            await ReplyAsync("Pong! I'm here!");
+        }
+
+        [Command("contribute")]
+        [Summary("Find out how you can contribute to NerdBot.")]
+        public async Task SendContribInfoAsync()
+        {
+            var embed = new EmbedBuilder()
+                {
+                    Title = "Contribute to NerdBot",
+                    Description = "Here is the information on how to contribute to NerdBot!",
+                    ThumbnailUrl = Context.Client.CurrentUser.GetAvatarUrl(),
+                    Color = new Color(0,76,161)
+                }
+                .AddField("Quotes",
+                    "Use the addquote command to add quotes.")
+                .AddField("Code",
+                    "If you like to write C#, let Ela know!")
+                .AddField("Hosting", "NerdBot is hosted on a VPS, and therefore costs to host. If you want to cover a month's hosting (about $5 or £4), you can support Ela on [Patreon here](https://patreon.com/elathedeveloper)")
+                .WithFooter("Thanks for helping out!")
+                .WithCurrentTimestamp()
+                .Build();
+            await ReplyAsync(embed: embed);
+        }
+
+        [Command("version")]
+        [Summary("Get NerdBot version information")]
+        public async Task SendVerInfoAsync()
+        {
+            await ReplyAsync($"NerdBot version {Assembly.GetExecutingAssembly().GetName().Version}. Running on {Environment.OSVersion.ToString()} (Processor Count (cores+threads): {Environment.ProcessorCount.ToString()})");
+        }
+
+        [Command("Uptime")]
+        [Summary("Get the current uptime of NerdBot")]
+        [Alias("ut", "up")]
+        public async Task SendUptimeAsync()
+        {
+            TimeSpan uptime = DateTime.UtcNow - Process.GetCurrentProcess().StartTime.ToUniversalTime();
+            await ReplyAsync($"NerdBot Uptime is currently {uptime.Days} days {uptime.Hours} hours {uptime.Minutes} minutes and {uptime.Seconds} seconds.");
+
+        }
+    }
+}
---
+++ SRC/NerdBot-Sharp/NerdBot/Modules/InfoModule.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+namespace NerdBot.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        public InfoModule(CommandService commandService)
+        {
+            CommandService = commandService;
+        }
+
+        public CommandService CommandService { get; set; }
+        
+        [Command("userinfo")]
+        [Summary
+            ("Returns info about the current user, or the user parameter, if one passed.")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync(
+            [Summary("The (optional) user to get info from")]
+            SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
+        }
+
+        [Command("about")]
+        [Summary("A little bit about NerdBot")]
+        public async Task AboutBotAsync()
+        {
+            await ReplyAsync(
+                "NerdBot. Written By ElaTheDeveloper - Contribute on GitHub! - We flew a kite in a public place!");
+        }
+        
+        
+        [Command("help")]
+        [Summary("What commands does NerdBot understand? Let's find out!")]
+        public async Task HelpCommand()
+        {
+
+            List<CommandInfo> commands = CommandService.Commands.ToList();
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder.WithAuthor(Context.Client.CurrentUser)
+                .WithColor(Color.Gold)
+                .WithDescription("These are all the commands I understand! Run one by adding ! before it.\n[Support NerdBot](https://patreon.com/elathedeveloper)")
+                .WithTitle("NerdBot Help")
+                .WithFooter($"{commands.Count} commands in database")
+                .WithCurrentTimestamp();
+
+            foreach (CommandInfo command in commands)
+            {
+                // Get the command Summary attribute information
+                string embedFieldText = command.Summary ?? "No description available\n";
+
+                embedBuilder.AddField(command.Name, embedFieldText);
+            }
+
+            await ReplyAsync("Here's a list of commands and their description: ", false, embedBuilder.Build());
+        }
+    }
+}
